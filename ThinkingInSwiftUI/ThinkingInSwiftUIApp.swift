4902d4eb23caffd28d4c9c5718a673bc97bf925e
mkotyak <kotyak.maria@gmail.com>
2022-08-11 11:11:29 +0300

    Initial Commit

---
+++ ThinkingInSwiftUI/ThinkingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingInSwiftUIApp.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Maria Kotyak on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ThinkingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
